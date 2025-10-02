b9991a575c0a8e51d4b2a4bb90dbfa8d0099f0cf
Connor Garey <connorgarey1999@gmail.com>
2025-10-02 22:11:24 +0100

    Window now allocated on the stack

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,13 @@
 
 int main (int argc, char *argv[]) {
   
-  std::unique_ptr<Window> game_window{ new Window(1280, 720, 60) };
+  // std::unique_ptr<Window> game_window{ new Window(1600, 900, 60) };
+  Window game_window { 1600, 900, 60 };
 
-  while (!game_window->shouldWindowClose()) {
-    game_window->beginDraw();
-    game_window->clearWindow();
-    game_window->endDraw();
+  while (!game_window.shouldWindowClose()) {
+    game_window.beginDraw();
+    game_window.clearWindow();
+    game_window.endDraw();
   }
   
   return 0;
