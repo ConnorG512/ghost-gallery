2be328bf899b2a6fbcb0dd8ce8c80c005603f84c
Connor Garey <connorgarey1999@gmail.com>
2025-10-03 14:18:12 +0100

    draws a background and cursor to the screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,28 @@
-#include "window.h"
 #include <memory>
+#include <raylib.h>
+
+#include "window.h"
 
 int main (int argc, char *argv[]) {
-  
+
   // std::unique_ptr<Window> game_window{ new Window(1600, 900, 60) };
   Window game_window { 1600, 900, 60 };
+
+  // Load background image
+  Texture2D background_sprite = LoadTexture("assets/image/default-background.png");
+  Texture2D cursor = LoadTexture("assets/image/ui/cursor-target.png");
 
   while (!game_window.shouldWindowClose()) {
     game_window.beginDraw();
     game_window.clearWindow();
+    DrawTexture(background_sprite, 0, 0, WHITE);
+    DrawTexture(cursor, GetMouseX(), GetMouseY(), WHITE);
     game_window.endDraw();
   }
-  
+
+  // Unload Texture
+  UnloadTexture(background_sprite);
+  UnloadTexture(cursor);
+
   return 0;
 }
