23f53f1a76116eac611a9d4bb5863927a31042ec
Connor Garey <connorgarey1999@gmail.com>
2025-10-03 23:26:09 +0100

    Moved initialiseApplication outside of the loop as it only needs to be done once.

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,9 @@ int main (int argc, char *argv[])
   // std::unique_ptr<Window> game_window{ new Window(1600, 900, 60) };
   GameManager game_manager { };
   
+  game_manager.initialiseApplication();
+  
   while (!game_manager.shouldGameClose()) {
-    game_manager.initialiseApplication();
     game_manager.inputUpdatePerTick();
     game_manager.drawUpdatePerTick();
   }
