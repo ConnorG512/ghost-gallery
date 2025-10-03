8b421c9cc5e96631f2bf8b45c1f05ca70834db56
Connor Garey <connorgarey1999@gmail.com>
2025-10-03 17:10:34 +0100

    Implemented lives and score

--- src/player.cpp
+++ src/player.cpp
@@ -1,19 +1,27 @@
 #include "player.h"
-#include <iostream>
-#include <print>
 
 Player::Player(int current_lives)
   : m_current_lives { current_lives } {};
 
 Player::~Player() {};
 
-int Player::calculateScore(int score_to_add) {
-  // TODO
-  std::println("STUB: calculateScore called!");
-  return 0;
+int Player::calculateScore(const int score_to_add) 
+{
+  m_current_score += score_to_add;
+
+  constexpr int maximum_score { 999999 };
+  if ( m_current_score > maximum_score ) 
+  {
+    m_current_score = maximum_score;
+  }
+  return m_current_score;
 };
-int Player::reduceLives(int num_to_reduce) {
-  // TODO
-  std::println("STUB: reduceLives called!");
-  return 0;
+int Player::reduceLives(const int num_to_reduce) 
+{
+  m_current_lives -= num_to_reduce;
+  if ( m_current_lives <= 0 )
+  {
+    m_current_lives = 0;
+  }
+  return m_current_lives;
 };
