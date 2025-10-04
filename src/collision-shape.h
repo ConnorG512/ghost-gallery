f1405e847b1d46bc7aa86b49ef1b318f6d9992ff
Connor Garey <connorgarey1999@gmail.com>
2025-10-04 22:42:22 +0100

    Made CollisionShape a struct for public access

--- src/collision-shape.h
+++ src/collision-shape.h
@@ -2,13 +2,11 @@
 
 #include <raylib.h>
 
-class CollisionShape 
+struct CollisionShape 
 {
-  public:
     CollisionShape();
     CollisionShape( const int x_pos, const int y_pos, const int width, const int height );
     bool isCollidingWith(const Rectangle& collider);
 
-  private:
     Rectangle m_collision_shape;
 };
