c9f980a1c403874da8aab271ed7974e1ee3b572e
Connor Garey <connorgarey1999@gmail.com>
2025-10-04 22:42:34 +0100

    Remove unneeded include

--- src/player.cpp
+++ src/player.cpp
@@ -1,5 +1,4 @@
 #include "player.h"
-#include "entity.h"
 
 Player::Player( const int pos_x, const int pos_y, const std::string& texture_path, const int coll_width, const int coll_height )
   : Entity{ pos_x, pos_y, texture_path, coll_width, coll_height } {}
