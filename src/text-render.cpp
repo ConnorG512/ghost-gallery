f6772d6302bc58fef8f31598610ae488a3b7e43a
Connor Garey <connorgarey1999@gmail.com>
2025-10-04 16:07:53 +0100

    Fixed missing TextRender::

--- src/text-render.cpp
+++ src/text-render.cpp
@@ -2,7 +2,7 @@
 #include <raylib.h>
 
 
-void drawTextToScreen(const std::string& displayed_text, const int pos_x, const int pos_y, const int size)
+void TextRender::drawTextToScreen(const std::string& displayed_text, const int pos_x, const int pos_y, const int size)
 {
   DrawText(displayed_text.c_str(), pos_x, pos_y, size, WHITE);
 }
