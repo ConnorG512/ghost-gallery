abe7e227f5c400a5286882ae6b9c6801bbf0f153
Connor Garey <connorgarey1999@gmail.com>
2025-10-11 16:17:29 +0100

    Text is now displayed centered on the screen.

--- src/gamestate/gamestate-splash.cpp
+++ src/gamestate/gamestate-splash.cpp
@@ -3,6 +3,7 @@
 #include "../input-handler.h"
 #include "../window.h"
 #include "../game-manager.h"
+#include <raylib.h>
 
 GameStateSplash::GameStateSplash( GameManager* game_manager, Window& game_window )
   : GameState { game_manager, game_window } {}
@@ -28,8 +29,7 @@ void GameStateSplash::gameplayLoop()
   m_game_window.clearWindow();
 
   m_background_image.drawSprite( 0 );
-  
-  TextRender::drawTextToScreen( "Press space key to start...", 1600 / 2 , 900 / 2, 32 );
+  drawSplashText();
 
   m_game_window.endDraw();
 }
@@ -38,3 +38,17 @@ void GameStateSplash::startGameplayLoop()
 {
   m_game_manager->changeCurrentGameState( GameManager::GameType::gameplay );
 };
+
+void GameStateSplash::drawSplashText()
+{
+  constexpr int font_size { 32 };
+  constexpr int screen_size_x { 1600 };
+  constexpr int screen_size_y { 900 };
+
+  const std::string& splash_text { "Press space key to start..." };
+  TextRender::drawTextToScreen( 
+    splash_text, screen_size_x / 2 - MeasureText(splash_text.c_str(), 32 ) / 2, 
+    screen_size_y / 2, 
+    font_size 
+  );
+}
--- src/gamestate/gamestate-splash.h
+++ src/gamestate/gamestate-splash.h
@@ -14,6 +14,7 @@ class GameStateSplash : public GameState
 
   protected:
     void startGameplayLoop();
+    void drawSplashText();
 
     ImageSprite m_background_image { { "assets/image/background/manor-1.png" }, 0, 0 };
 
