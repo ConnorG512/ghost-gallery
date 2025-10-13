97ec6c9bb3243562713be126644a28e132d15321
Connor Garey <connorgarey1999@gmail.com>
2025-10-14 00:58:59 +0100

    Formatting

--- src/text-render.cpp
+++ src/text-render.cpp
@@ -8,12 +8,12 @@ namespace
   constexpr int default_font_size { 32 };
 }
 
-void TextRender::drawTextToScreen(const std::string& displayed_text, const int pos_x, const int pos_y, const int size)
+void TextRender::drawTextToScreen( const std::string& displayed_text, const int pos_x, const int pos_y, const int size )
 {
   DrawText(displayed_text.c_str(), pos_x, pos_y, size, WHITE);
 }
 
-void TextRender::drawTextToScreen(const std::string& displayed_text, const int pos_x, const int pos_y )
+void TextRender::drawTextToScreen( const std::string& displayed_text, const int pos_x, const int pos_y )
 {
   DrawText(displayed_text.c_str(), pos_x, pos_y, default_font_size, WHITE);
 }
