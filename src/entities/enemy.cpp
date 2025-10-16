9313a7f5567186061197982822415fc7576c11f7
Connor Garey <connorgarey1999@gmail.com>
2025-10-16 16:51:22 +0100

    Enemy rewrite.

--- src/entities/enemy.cpp
+++ src/entities/enemy.cpp
@@ -1,15 +1,62 @@
 #include "enemy.h"
+#include "player.h"
+#include "../random-generation.h"
+#include "../input-handler.h"
+#include <array>
+
+namespace 
+{
+  constexpr std::array<int , 2> score_range { 25, 75 };
+}
 
 Enemy::Enemy
 (
-    
-  const std::vector<std::string>& texture_paths,
-  const int max_health,
-  const int current_health,
   const int x_pos,
-  const int y_pos,
-  const int score_to_give 
+  const int y_pos
 ) 
-  : Entity { texture_paths, max_health, current_health, x_pos, y_pos }
-  , m_score_to_give { score_to_give } {}
+  : Entity { { "assets/image/entity/enemy/ghost/ghost-1.png" }, 2, 2, x_pos, y_pos }
+  , score_to_give { RandomGeneration::NumberBetween( score_range.at( 0 ), score_range.at( 1 ))} { }
 
+void Enemy::attackPlayer( Player& current_player )
+{
+  if ( m_tick_component.hasHitTickThreshold()) {
+    current_player.health_component.reduceHealth( damage_component.CalculateDamage());
+    respawnEnemy();
+  }
+}
+
+void Enemy::respawnEnemy()
+{
+  moveSprite(RandomGeneration::NumberBetween( 100, 1400 ), RandomGeneration::NumberBetween( 200, 750 )); 
+  changeEnemyGivenScore();
+  m_tick_component.resetTickCount();
+}
+
+void Enemy::collidedWithPlayer( Player& current_player )
+{
+  if ( collision.isCollidingWith( current_player.collision.m_collision_shape ))
+  {
+    current_player.sprite.drawSprite( 1 );
+
+    using enum InputHandler::ButtonPress;
+    if ( InputHandler::receiveInput() == left_mouse )
+    {
+      respawnEnemy();
+      current_player.score_component.increaseScore( score_to_give );
+    }
+  }
+}
+
+void Enemy::incrementAttackClock( Player& current_player )
+{
+  m_tick_component.incrementTickCount();
+  if ( m_tick_component.hasHitTickThreshold())
+  {
+    attackPlayer( current_player );
+  }
+}
+
+void Enemy::changeEnemyGivenScore()
+{
+  score_to_give = RandomGeneration::NumberBetween( score_range.at( 0 ), score_range.at( 1 ));
+}
--- src/entities/enemy.h
+++ src/entities/enemy.h
@@ -2,22 +2,29 @@
 
 #include "entity.h"
 #include "components/damage-component.h"
+#include "components/tick-component.h"
 
+class Player;
 
 class Enemy : public Entity 
 {
   public:
     Enemy
     ( 
-      const std::vector<std::string>& texture_paths,
-      const int max_health,
-      const int current_health,
-      const int x_pos,
-      const int y_pos,
-      const int score_to_give = 30
+      const int x_pos = 0,
+      const int y_pos = 0
     );
+    
+    void respawnEnemy();
+    void collidedWithPlayer( Player& current_player );
+    void incrementAttackClock( Player& current_player );
 
+    DamageComponent damage_component { 2, 10 };
+    int score_to_give { 30 };
 
-  protected:
-    const int m_score_to_give { 30 };
+  private: 
+    void changeEnemyGivenScore();
+    void attackPlayer( Player& current_player );
+    
+    TickComponent m_tick_component { };
 };
