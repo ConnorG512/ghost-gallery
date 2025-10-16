1d3063339c55cf85094e26654a77d88c56c67999
Connor Garey <connorgarey1999@gmail.com>
2025-10-16 12:45:58 +0100

    Simplified collision return

--- src/collision-shape.cpp
+++ src/collision-shape.cpp
@@ -1,24 +1,13 @@
 #include "collision-shape.h"
 
-#include <format>
-#include <iostream>
 #include <raylib.h>
 
-CollisionShape::CollisionShape() {};
-
 CollisionShape::CollisionShape( const int x_pos, const int y_pos, const int width, const int height )
-  : m_collision_shape { (float)x_pos, (float)y_pos, (float)width, (float)height} 
-{
-  std::cout << std::format("{}: {},{},{},{}", "Collision shape: ", m_collision_shape.x, m_collision_shape.y, m_collision_shape.width, m_collision_shape.height ) << std::endl;
-}
+  : m_collision_shape { (float)x_pos, (float)y_pos, (float)width, (float)height} { }
 
 bool CollisionShape::isCollidingWith(const Rectangle& collider)
 {
-  if (CheckCollisionRecs(m_collision_shape, collider))
-  {
-    return true;
-  }
-  return false;
+  return CheckCollisionRecs( m_collision_shape, collider );
 }
 
 void CollisionShape::updateCollisionPosition( const int pos_x, const int pos_y )
