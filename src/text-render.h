a956745b984b322f8fe9b31f2e2b9b7b2d90a238
Connor Garey <connorgarey1999@gmail.com>
2025-10-16 21:45:51 +0100

    Removed text render

--- src/text-render.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-#include <raylib.h>
-
-namespace TextRender 
-{
-  void drawTextToScreen(const std::string& displayed_text, const int pos_x = 0, const int pos_y = 0, const int size = 32 );
-}
