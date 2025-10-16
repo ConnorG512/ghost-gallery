8cbf4e225bdfc9ef5b3b3b11fd8b829d8674eaee
Connor Garey <connorgarey1999@gmail.com>
2025-10-16 11:45:58 +0100

    game ui now prints the correct values and text

--- src/game-ui.h
+++ src/game-ui.h
@@ -1,14 +1,20 @@
 #pragma once 
 
+#include <vector>
+#include <string>
+
+class Player;
+class ScoreManager;
+
 class GameUi 
 {
   public:
-    GameUi( int starting_pos_x = 0, int starting_pos_y = 0, int font_size = 32, int text_spacing = 10 );
-    void drawAll();
+    GameUi( const int starting_pos_x = 0, const int starting_pos_y = 0, const int font_size = 32, const int text_spacing = 40 );
+    void drawAll( const std::array<int, 3>& game_props );
 
   private:
     int m_starting_pos_x { 0 };
     int m_starting_pos_y { 0 };
     int m_font_size { 32 };
-    int m_text_spacing { 10 };
+    int m_text_spacing { 40 };
 };
