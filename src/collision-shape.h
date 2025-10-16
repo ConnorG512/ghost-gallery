16b89c8c4414fcbb768e040196a690b2cb0b88d7
Connor Garey <connorgarey1999@gmail.com>
2025-10-16 18:25:30 +0100

    Collision initialiser takes default arguements

--- src/collision-shape.h
+++ src/collision-shape.h
@@ -2,10 +2,10 @@
 
 #include <raylib.h>
 
-struct CollisionShape 
+class CollisionShape 
 {
-    CollisionShape();
-    CollisionShape( const int x_pos, const int y_pos, const int width, const int height );
+  public:
+    CollisionShape( const int x_pos, const int y_pos, const int width = 128, const int height = 128 );
     
     bool isCollidingWith(const Rectangle& collider);
     void updateCollisionPosition( const int pos_x, const int pos_y );
