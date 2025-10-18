02fdec5cc7e999006bf9faf307b98dfb833af5ef
Connor Garey <connorgarey1999@gmail.com>
2025-10-18 22:28:48 +0100

    gameplay-ui now uses std::views::zip

--- src/ui/ui-gameplay.cpp
+++ src/ui/ui-gameplay.cpp
@@ -1,6 +1,7 @@
 #include "ui-gameplay.h"
 #include <array>
 #include <format>
+#include <ranges>
 #include <raylib.h>
 
 UiGameplay::UiGameplay(const int start_pos_x, const int start_pos_y,
@@ -19,11 +20,12 @@ void UiGameplay::drawUi(const std::array<int, 3> &player_stats)
     };
 
     int text_y_pos{m_start_position_y};
-    for (int i = 0; i < ui_strings.size(); ++i)
+
+    for (auto [current_string, player_stat] :
+         std::views::zip(ui_strings, player_stats))
     {
-        DrawText(
-            std::format("{}: {}", ui_strings.at(i), player_stats.at(i)).c_str(),
-            m_start_position_x, text_y_pos, m_font_size, WHITE);
+        DrawText(std::format("{}: {}", current_string, player_stat).c_str(),
+                 m_start_position_x, text_y_pos, m_font_size, WHITE);
         text_y_pos += m_spacing;
     }
 }
