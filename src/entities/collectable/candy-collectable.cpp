622bfa95bc844000cdd052e994de784e49831cfb
Connor Garey <connorgarey1999@gmail.com>
2025-10-21 22:05:04 +0100

    Fixing path to the candy collectable sprite

--- src/entities/collectable/candy-collectable.cpp
+++ src/entities/collectable/candy-collectable.cpp
@@ -4,7 +4,8 @@
 #include "collectable.h"
 
 CandyCollectable::CandyCollectable(const int x_pos, const int y_pos, const int multiplier_amount)
-    : Collectable({"assets/image/entity/candy"}, 2, 2, x_pos, y_pos, true), m_multiplier_to_give{multiplier_amount}
+    : Collectable({"assets/image/entity/candy/candy.png"}, 2, 2, x_pos, y_pos, true),
+      m_multiplier_to_give{multiplier_amount}
 {
 }
 
