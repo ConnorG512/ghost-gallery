a6609a3b8830673cbd3e8b44fa5435fdb448884b
Connor Garey <connorgarey1999@gmail.com>
2025-10-21 21:07:48 +0100

    Created constructor, destructor and virtual function

--- src/entities/collectable/candy-collectable.cpp
+++ src/entities/collectable/candy-collectable.cpp
@@ -1,1 +1,7 @@
 #include "candy-collectable.h"
+#include "collectable.h"
+
+CandyCollectable::CandyCollectable(const int x_pos, const int y_pos, const int multiplier_amount)
+    : Collectable({"assets/image/entity/candy"}, 2, 2, x_pos, y_pos, true), m_multiplier_to_give{multiplier_amount}
+{
+}
--- src/entities/collectable/candy-collectable.h
+++ src/entities/collectable/candy-collectable.h
@@ -1,9 +1,16 @@
 #pragma once
 
 #include "collectable.h"
+#include <vector>
 
 class CandyCollectable : public Collectable
 {
-    CandyCollectable();
-    ~CandyCollectable() = default;
+  public:
+    CandyCollectable(const int x_pos, const int y_pos, const int multiplier_amount);
+    virtual ~CandyCollectable() = default;
+
+    virtual void givePowerUp(Player& current_player);
+
+  private:
+    int m_multiplier_to_give { 2 };
 };
