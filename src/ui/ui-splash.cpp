e43f3ef3f8bf871136845024d52924110ed880cf
Connor Garey <connorgarey1999@gmail.com>
2025-10-25 11:17:10 +0100

    Splash text is now a for loop.
    formatting

--- src/ui/ui-splash.cpp
+++ src/ui/ui-splash.cpp
@@ -18,15 +18,9 @@ void UiSplash::drawUi(const int& player_high_score)
 
     int text_y_pos{m_start_position_y};
 
-    DrawText(splash_titles.at(0), m_start_position_x, text_y_pos, m_font_size, WHITE);
-    text_y_pos += m_spacing;
-
-    DrawText(splash_titles.at(1), m_start_position_x, text_y_pos, m_font_size, WHITE);
-    text_y_pos += m_spacing;
-
-    DrawText(std::format("{}: {}", splash_titles.at(2), std::to_string(player_high_score)).c_str(),
-             m_start_position_x,
-             text_y_pos,
-             m_font_size,
-             WHITE);
+    for (auto const current_string : splash_titles)
+    {
+        DrawText(current_string, m_start_position_x, text_y_pos, m_font_size, WHITE);
+        text_y_pos += m_spacing;
+    }
 }
