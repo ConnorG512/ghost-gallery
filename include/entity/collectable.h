d0b4da9325de60c648a96826cc5cac34cc3d0d90
Connor Garey <connorgarey1999@gmail.com>
2025-10-28 14:44:03 +0000

    Moved the collectable constructor to protected.

--- include/entity/collectable.h
+++ include/entity/collectable.h
@@ -7,7 +7,10 @@ class Player;
 class Collectable : public Entity
 {
   public:
-    Collectable(const std::vector<std::string>& texture_paths, const int x_pos = 0, const int y_pos = 0);
     virtual ~Collectable() = default;
+
     virtual void givePowerUp(Player& current_player) = 0;
+
+  protected:
+    Collectable(const std::vector<std::string>& texture_paths, const int x_pos = 0, const int y_pos = 0);
 };
